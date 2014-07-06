a615f66a35c9ca6ce9ac67418c6deb6f79726346
James Horey <jlh@opencore.io>
2014-07-06 23:09:19 +0000

    try changing db

--- config/database.js
+++ config/database.js
@@ -1,5 +1,5 @@
 module.exports = {
 
 	// the database url to connect
-	url : 'mongodb://' + process.env.BACKEND_STORAGE_MONGO_USER + ':' + process.env.BACKEND_STORAGE_MONGO_PASS + '@' + process.env.BACKEND_STORAGE_MONGO + ':27017/todo'
+	url : 'mongodb://' + process.env.BACKEND_STORAGE_MONGO_USER + ':' + process.env.BACKEND_STORAGE_MONGO_PASS + '@' + process.env.BACKEND_STORAGE_MONGO + ':27017/admin'
 }
