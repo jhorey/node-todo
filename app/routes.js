05bef1f3e07fd4c9352316048da2a1435ffe7f07
James Horey <jlh@opencore.io>
2014-07-06 22:58:40 +0000

    add server log msgs

--- app/routes.js
+++ app/routes.js
@@ -5,13 +5,16 @@ module.exports = function(app) {
 	// api ---------------------------------------------------------------------
 	// get all todos
 	app.get('/api/todos', function(req, res) {
+	    console.log("get all todos");
 
 		// use mongoose to get all todos in the database
 		Todo.find(function(err, todos) {
 
 			// if there is an error retrieving, send the error. nothing after res.send(err) will execute
-			if (err)
-				res.send(err)
+		    if (err) {
+			console.log(err);
+			res.send(err)
+		    }
 
 			res.json(todos); // return all todos in JSON format
 		});
@@ -19,19 +22,23 @@ module.exports = function(app) {
 
 	// create todo and send back all todos after creation
 	app.post('/api/todos', function(req, res) {
-
+	    console.log("post todo");
 		// create a todo, information comes from AJAX request from Angular
 		Todo.create({
 			text : req.body.text,
 			done : false
 		}, function(err, todo) {
-			if (err)
-				res.send(err);
+			if (err) {
+			    console.log(err);
+			    res.send(err);
+			}
 
 			// get and return all the todos after you create another
 			Todo.find(function(err, todos) {
-				if (err)
-					res.send(err)
+				if (err) {
+				    console.log(err);				    
+				    res.send(err)
+				}
 				res.json(todos);
 			});
 		});
@@ -40,6 +47,7 @@ module.exports = function(app) {
 
 	// delete a todo
 	app.delete('/api/todos/:todo_id', function(req, res) {
+	    console.log("delete todo");
 		Todo.remove({
 			_id : req.params.todo_id
 		}, function(err, todo) {
